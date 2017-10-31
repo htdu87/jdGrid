40badabfd8482ff66aeed891636eb092e52eb797
htdu87 <huynhthanhdu@gmail.com>
2017-10-31 15:26:48 +0700

    cm

--- js/basic-demo.js
+++ js/basic-demo.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 		]
 	});
 	
-	/* g.data('jdgrid').fillData([
+	 g.data('jdgrid').fillData([
 		{name:'Airi Satou',position:'Accountant',office:'Tokyo',age:33,date:'2008/11/28',salary:'162700'},
 		{name:'Ashton Cox',position:'Chief Executive Officer (CEO)',office:'London',age:47,date:'2009/10/09',salary:1200000},
 		{name:'Bradley Greer',position:'Junior Technical Author',office:'San Francisco',age:66,date:'2009/01/12',salary:86000}
@@ -31,7 +31,7 @@ $(document).ready(function(){
 		{name:'Otto Clay',age:61,address:'911-5143 Luctus Ave',married:true},
 		{name:'Timothy Henson',age:78,address:'P.O. Box 738, 7583 Quisque St.',married:false},
 		{name:'Ramona Benton',age:43,address:'847-4303 Dictum Av.',married:true}
-	]); */
+	]); 
 	
 	$('#btnRefresh').click(function(){
 		$('.jdgrid').each(function(i,o){
