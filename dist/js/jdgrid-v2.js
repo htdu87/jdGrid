cbeceaf1368d96636dd054ad557f46ca39d55294
htdu87 <huynhthanhdu@gmail.com>
2018-02-21 23:12:27 +0700

    cm

--- dist/js/jdgrid-v2.js
+++ dist/js/jdgrid-v2.js
@@ -40,11 +40,17 @@
 			var tblFooter=$('<table><tfoot><tr></tr></tfoot></table>').addClass('table table-bordered table-condensed table-hover');
 			
 			$.each(options.columns,function(i,col){
-				var th=$('<th></th>').html(col.title);
+				var colStyle=$.extend({},$.fn.jdGrid.defaultColumnStyles,col);
+				var style={'text-align':colStyle.align,'width':colStyle.width,'color':colStyle.color};
+				var th=$('<th></th>').html(col.title).css(style);
 				tblHeader.find('thead tr').append(th);
 				tblBody.find('thead tr').append(th.clone());
 				var td=$('<td>');
 				tblFooter.find('tfoot tr').append(td);
+			});
+			
+			$.each(options.data,function(i,obj){
+				tblBody.find('tbody').append(createDataRow(obj));
 			});
 			
 			headerWpr.append(tblHeader);
@@ -54,10 +60,64 @@
 			$(this).append(headerWpr).append(bodyWpr).append(footerWpr).css({'border':'1px solid '+options.borderClr});
 			$(this).data('jdgrid',jdgrid);
 			
-			
+			adjGrid($(this));
 		});
 		return $(this);
+		
+		function createDataRow(rowData){
+			var tr=$('<tr></tr>');
+			$.each(options.columns,function(i,col){
+				var colStyle=$.extend({},$.fn.jdGrid.defaultColumnStyles,col);
+				var style={'text-align':colStyle.align,'width':colStyle.width,'color':colStyle.color,'font-weight':colStyle.bold};
+				var td=$('<td></td>').css(style);
+				switch(colStyle.type){
+					case 'money':
+						td.html(formatNum(rowData[col.name]));
+						break;
+					case 'date':
+						td.html('<i>Deprecated</i>');
+						break;
+					case 'interval':
+						td.html(milisecToDate(rowData[col.name]));
+						break;
+					case 'base64img':
+						td.html('<i>Deprecated</i>');
+						break;
+					case 'control':
+						td.html(col.content(rowData));
+						break;
+					case 'check':
+						var checked=(rowData[col.name]==1||rowData[col.name]==true)?'checked':'';
+						td.html('<input type="checkbox" disabled="disabled" '+checked+' />');
+						break;
+					default:
+						td.html(rowData[col.name]);
+				}
+				tr.append(td);
+			});
+			return tr;
+		}
+		
+		function adjGrid(jqObj){
+			if(jqObj.find('.jdgrid-body-wrapper').length>0 && jqObj.find('.jdgrid-header-wrapper').length>0){
+				jqObj.find('.jdgrid-body-wrapper table thead tr:first th').each(function(i){
+					var w=$(this).outerWidth();
+					jqObj.find('.jdgrid-header-wrapper table thead tr:first th:nth-child('+(i+1)+'),.jdgrid-footer-wrapper table tfoot tr:first td:nth-child('+(i+1)+')').outerWidth(w);
+				});
+				jqObj.find('.jdgrid-body-wrapper table').css('margin-top','-'+jqObj.find('.jdgrid-header-wrapper table thead').outerHeight()+'px');
+			}
+		}
+		
+		function formatNum(num) {
+			return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, options.thousandSeparate);
+		}
+		
+		function milisecToDate(interval){
+			var date=new Date(interval);
+			return ('0'+date.getDate()).slice(-2) + '/' +  ('0'+(date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear();
+		}
 	};
+	
 	$.fn.jdGrid.defaultOptions={
 		height:'300px',
 		columns:[],
@@ -75,8 +135,9 @@
 	$.fn.jdGrid.defaultColumnStyles={
 		align:'left',
 		width:'auto',
-		textClr:'inherit',
-		bold:false
+		color:'inherit',
+		bold:'inherit',
+		type:'text'
 	}
 	
 	
--- js/basic-demo.js
+++ js/basic-demo.js
@@ -2,14 +2,21 @@ $(document).ready(function(){
 	var g=$('#jdgrid').jdGrid({
 		columns:[
 			{name:'name',title:'Name'},
-			{name:'position',title:'Position'},
+			{name:'position',title:'Position', color:'red'},
 			{name:'office',title:'Office'},
 			{name:'age',title:'Age',align:'right'},
 			{name:'date',title:'Start date'},
 			{name:'salary',title:'Salary',align:'right',type:'money'}
 		],
 		borderClr:'red',
-		footer:true
+		footer:true,
+		data:[
+			{name:'Airi Satou',position:'Accountant',office:'Tokyo',age:33,date:'2008/11/28',salary:'162700'},
+			{name:'Ashton Cox',position:'Chief Executive Officer (CEO)',office:'London',age:47,date:'2009/10/09',salary:1200000},
+			{name:'Bradley Greer',position:'Junior Technical Author',office:'San Francisco',age:66,date:'2009/01/12',salary:86000}
+		],
+		decimalSeparate:',',
+		thousandSeparate:'.'
 	});
 	
 	var g1=$('#jdgrid1').jdGrid({
@@ -20,6 +27,11 @@ $(document).ready(function(){
 			{name:'address',title:'Address'},
 			{name:'married',title:'Is married',align:'center',type:'check'},
 			{name:'control',title:'Actions',align:'center',type:'control',content:function(i,item){return '<a href="#">Link</a>'}}
+		],
+		data:[
+			{name:'Otto Clay',age:61,address:'911-5143 Luctus Ave',married:true},
+			{name:'Timothy Henson',age:78,address:'P.O. Box 738, 7583 Quisque St.',married:false},
+			{name:'Ramona Benton',age:43,address:'847-4303 Dictum Av.',married:true}
 		]
 	});
 	
@@ -43,7 +55,7 @@ $(document).ready(function(){
 		}
 	}); */
 	
-	 g.data('jdgrid').fillData([
+	g.data('jdgrid').fillData([
 		{name:'Airi Satou',position:'Accountant',office:'Tokyo',age:33,date:'2008/11/28',salary:'162700'},
 		{name:'Ashton Cox',position:'Chief Executive Officer (CEO)',office:'London',age:47,date:'2009/10/09',salary:1200000},
 		{name:'Bradley Greer',position:'Junior Technical Author',office:'San Francisco',age:66,date:'2009/01/12',salary:86000}
